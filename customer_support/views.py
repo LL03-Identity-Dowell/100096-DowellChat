e2ebe5a8dcaa1d293461b4a24694c98b108fba98
Rahul Tadola <rahultadola@gmail.com>
2023-07-25 13:19:54 +0530

    umairs view

--- customer_support/views.py
+++ customer_support/views.py
@@ -311,6 +311,7 @@ def index(request):
 
 def portfolio_control(d_user, session_id, is_staff):
     try:
+        
         portfolio = Portfolio.objects.get(userID=d_user["userinfo"]["userID"], organization = d_user["portfolio_info"][0]["org_id"])
     except Portfolio.DoesNotExist:
         portfolio = Portfolio.objects.create(
@@ -529,20 +530,13 @@ def create_room_sales_agent(request, *args, **kwargs):
 @csrf_exempt
 def create_room_sales_agent(request, *args, **kwargs):
     if request.method == "POST":
-        session_id = request.POST.get('session_id', None)
+        data = json.loads(request.body)
+        session_id = data.get('session_id')
+        sub_product = data.get('sub_product', '')  # Extract the sub_product from the request
+        product = kwargs['product'].lower()
         url = 'https://100014.pythonanywhere.com/api/userinfo/'
         response = requests.post(url, data={'session_id': session_id})
-        print(response)
-        d_user = response.json()  # Extract the JSON data from the response
-        print(d_user)
-        product = kwargs['product'].lower()
-        print("Product: ", product)
-        sub_product = request.POST.get('sub_product', '')  # Extract the sub_product from the request
-        print("subproduct: ", sub_product)
-
-        #print(type(d_user))
-        #print("Product get args: ", session_id, product, d_user)
-
+        d_user = response.json()
         portfolio = portfolio_control(d_user, session_id, False)
         print("portfolio: ", portfolio)
         room, messages = room_control_sales_agent(portfolio, product.lower(), sub_product.lower())  # Call the updated room_control_sales_agent
@@ -588,19 +582,19 @@ def send_msg_api_3(request, pk):
             pass
 
     print("Send Message 3 : ", user_id , " - ", org_id, "message : ", message, request.POST)
+    import pdb;pdb.set_trace()
     if user_id:
-
         if message :
             try:
                 msg_type = message_type if message_type else "TEXT"
                 portfolio = Portfolio.objects.get(userID=str(user_id), organization=str(org_id))
-                side = False if room.sender_portfolio == portfolio else True
+                sd = False if room.sender_portfolio == portfolio else True
                 msg = Message.objects.create(
                     room=room,
                     message=message,
                     author=portfolio,
                     read=False,
-                    side=side,
+                    side=sd,
                     message_type=msg_type
                 )
                 msg.save()
