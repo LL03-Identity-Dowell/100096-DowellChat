837bf4b26b563cf244554b5a3cf7100f7842682a
Rahul Tadola <rahultadola@gmail.com>
2023-08-31 10:24:40 +0530

    corrected errors

--- app/urls.py
+++ app/urls.py
@@ -12,3 +12,4 @@ urlpatterns = [
     # ""  QR code API methods""
     path('create_master_link/<str:workspace_id>/', QRServiceHandler.as_view(), name='create_master_link'),
     path('init/<str:org_id>/<str:product_name>/<str:user_id>/', QRServiceValidationHandler.as_view(), name='validate-public-link'),
+]
