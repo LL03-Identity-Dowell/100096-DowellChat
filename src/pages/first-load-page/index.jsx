c40f1db0ba8173dc95bd3c761aff527f2a90562f
aliahmarawan8 <aliahmarawan8@gmail.com>
2023-10-12 22:44:20 +0500

    loader is added

--- src/pages/first-load-page/index.jsx
+++ src/pages/first-load-page/index.jsx
@@ -4,7 +4,6 @@ import DataContext from "../../context/data-context";
 import { CustomerSupport } from "../customer-support";
 import { LivingLabChat } from "../living-lab-chat";
 import axios from "axios";
-import { RoomLoader } from "../../components/room-loader";
 // import SecondRoute from "../secondRoute";
 
 export const FirstLoadComponent = () => {
@@ -297,49 +296,12 @@ export const FirstLoadComponent = () => {
   ) : livingLabChatEligible ? (
     <LivingLabChat />
   ) : (
-    <main>
-      <svg
-        class="ip"
-        viewBox="0 0 256 128"
-        width="256px"
-        height="128px"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <linearGradient id="grad1" x1="0" y1="0" x2="1" y2="0">
-            <stop offset="0%" stop-color="#5ebd3e" />
-            <stop offset="33%" stop-color="#ffb900" />
-            <stop offset="67%" stop-color="#f78200" />
-            <stop offset="100%" stop-color="#e23838" />
-          </linearGradient>
-          <linearGradient id="grad2" x1="1" y1="0" x2="0" y2="0">
-            <stop offset="0%" stop-color="#e23838" />
-            <stop offset="33%" stop-color="#973999" />
-            <stop offset="67%" stop-color="#009cdf" />
-            <stop offset="100%" stop-color="#5ebd3e" />
-          </linearGradient>
-        </defs>
-        <g fill="none" stroke-linecap="round" stroke-width="16">
-          <g class="ip__track" stroke="#ddd">
-            <path d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56" />
-            <path d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64" />
-          </g>
-          <g stroke-dasharray="180 656">
-            <path
-              class="ip__worm1"
-              stroke="url(#grad1)"
-              stroke-dashoffset="0"
-              d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56"
-            />
-            <path
-              class="ip__worm2"
-              stroke="url(#grad2)"
-              stroke-dashoffset="358"
-              d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64"
-            />
-          </g>
-        </g>
-      </svg>
-    </main>
+    <div className="flex justify-center items-center w-full h-screen">
+      {window.location.hash.includes("living-lab-chat") ? (
+        <span className="livingLab">Living Lab Chat</span>
+      ) : (
+        <span className="customerSupport">Customer Support</span>
+      )}
+    </div>
   );
 };
